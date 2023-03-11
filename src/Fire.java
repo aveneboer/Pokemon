954cb602b9f0b613165a7d23c72c1667b51262d8
aveneboer <anoukveneboer@hotmail.com>
2023-03-11 18:36:59 +0100

    finale uitwerkingen

--- src/Fire.java
+++ src/Fire.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Fire {
+public class Fire extends Pokemon {
+    private int defends;
+    private int weight;
+
+
+    public Fire(String type, String name, String food, int hp, int defends, int weight, String sound) {
+        super(type, name, food, hp, sound);
+        this.defends = defends;
+        this.weight = weight;
+
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getDefends() {
+        return defends;
+    }
+
+    public void setDefends(int defends) {
+        this.defends = defends;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("This Pokemon burns wood and eats the ashes");
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Het type Fire brandt");
+    }
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Pokemon {
+public abstract class Pokemon {
+    private String name;
+    private String food;
+    public int hp;
+    public String type;
+    public String sound;
+
+
+    public Pokemon(String type, String name, String food, int hp, String sound) {
+        this.type = type;
+        this.food = food;
+        this.name = name;
+        this.hp = hp;
+        this.sound = sound;
+
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Food: " + food);
+        System.out.println("HP: " + hp);
+    }
+
+    public void makesSound() {
+        System.out.println("This Pokemon makes the following sound: " + sound);
+    }
+
+    public abstract void eat();
+
+    public abstract void makeNoise();
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
